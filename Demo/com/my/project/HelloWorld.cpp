379821af68e22126d98d8e61450d5c425637b861
A2Z-n2e <627315447@qq.com>
2022-05-05 17:31:08 +0800

    First Day

---
+++ Demo/com/my/project/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Administrator on 2022/5/4.
+//
+//头文件<iostream>
+#include <iostream>
+//使用std命名空间【其他具体命名空间 & 是否能自己设置新的命名空间？】
+using namespace std;
+
+int main(){
+    //输出语句：cout <<
+    cout << "Hello,World!";
+    //终止main()函数并向调用进程返回0
+    return 0;
+}
+
